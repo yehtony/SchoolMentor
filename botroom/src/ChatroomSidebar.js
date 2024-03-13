1f17fb0a27ee7f616bff028c36367b755fbe6eb9
Wang <gavin19950511@gmail.com>
2024-03-13 00:49:41 +0000

    update(web): Update tips interface

--- botroom/src/ChatroomSidebar.js
+++ botroom/src/ChatroomSidebar.js
@@ -3,18 +3,18 @@ import React from 'react';
 
 const ChatroomSidebar = () => {
   const tips = [
-    { id: 1, title: 'conversation from a kid:' , content: " “like, nuclear power is like, when we use tiny, tiny things called atoms to make electricity.It's like having a super special toy that makes lots and lots of power.But some people say it's not good because it can be a little bit dangerous, like when you play with toys that might break.So, um, do I agree with having nuclear power as the main electric thingy in Taiwan?I think it's like deciding which flavor of ice cream is the best.some people like chocolate, and some people like vanilla.Maybe grown-ups can talk and think really hard to decide what's safest and best for everyone in Taiwan. It's like picking the yummiest ice cream, but way more serious!" },
-    // Add more rooms as needed
-  ];
+    { id: 1, title: '磁鐵' , content: "能知道磁鐵的發現科學史" },
+    { id: 2, title: '磁鐵與磁力' , content: " “磁鐵的兩極為N極和S極，磁極間的磁力是一種超距力" },
+    { id: 3, title: '磁力線與磁場' , content: "磁力線是假想線，可描述磁場的大小及方向" },  ];
   
   return (
     <div className="sidebar">
-      <h2>Paragraph Question:<p style={{color: '#aa3333'}}>Do you agree with development the nuclear power as main electric source in Taiwan?</p></h2>
+      <h2>After the class <p >Do you have questions about following units?</p></h2>
       <ul>
         {tips.map(tip => (
           <li>
             <div key={tip.id} >
-              <p>{tip.title}</p>
+              <p style={{color: '#aa3333'}}>{tip.title}</p>
               <p>{tip.content}</p>
             </div>
           </li>
